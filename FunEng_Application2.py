ced6c591d64a15b9ed8cde9cee1d2ecf82e83875
Sathika Limawiruchaphong <s7070126@kmitl.ac.th>
2014-12-11 22:05:39 +0700

    Dictionary
    add new words and debug the color of BG

--- FunEng_Application2.py
+++ FunEng_Application2.py
@@ -1,16 +1,54 @@
+# -*- coding: UTF-8 -*-
 '''FunEng Application'''
 from Tkinter import *
 import random
 root = Tk()
 #Head
-
-
-dct = {'cat':'Animal that have two triangle ear', \
-       'dog':'That sound box box',\
-       'cafeeyen':'cat of IT ladkrabang', \
-       'poji':'a very bad slime'}
+'''
+English practice game
+'''
+point = 0
+dct = {'expect':'คาดหวัง', \
+       'energetic':'กระปรี้กระเปร่า',\
+       'effect':'ผลกระทบ', \
+       'irony':'การถากถาง', \
+       'kinetic':'เกี่ยวกับการเคลื่อนไหว',\
+       'nanotechnology':'นาโนเทคโนโลยี',\
+       'nervous':'ประหม่า',\
+       'plasma':'ส่วนที่เป็นนํ้าสีเหลืองในเลือด',\
+       'reparation':'การซ่อมแซม',\
+       'respiration':'การหายใจ',\
+       'taxonomy':'การจัดแบ่งสิ่งมีชีวิตออกเป็นกลุ่มต่างๆ',\
+       'vortex':'กระแสลม หรือกระแสนํ้าที่หมุนวน ',\
+       'signature':'ลงลายมือชื่อ',\
+       'terminal':'สถานี',\
+       'crew':'ลูกเรือ',\
+       'customs':'ศุลกากร',\
+       'florist':'ร้านขายดอกไม้',\
+       'patient':'อดทน',\
+       'Mental':'เกี่ยวกับจิตใจ',\
+       'acceptable':'ยอมรับได้',\
+       'nonsense':'เรื่องไร้สาระ',\
+       'obedience':'การเชื่อฟังคำสั่ง',\
+       'photography':'การถ่ายภาพ',\
+       'possibility':'ความเป็นไปได้',\
+       'quantity':'ปริมาณ',\
+       'quality':'คุณภาพ',\
+       'refuse':'ปฏิเสธ',\
+       'religion':'ศาสนา',\
+       'satisfy':'ทำให้พอใจ',\
+       'secretary':'เลขานุการ',\
+       'shelter':'ที่หลบภัย',\
+       'shopkeeper':'เจ้าของร้าน',\
+       'similar':'ที่คล้ายกัน',\
+       'transparent':'โปร่งใส',\
+       'urgent':'ด่วน',\
+       'wages':'ค่าจ้าง',\
+       'wander':'ร่อนเร่',\
+       'librarian':'บรรณารักษ์',\
+       'landlord':'เจ้าของที่ดิน',\
+       'memorial':'อนุสรณ์'}
 wrd = random.choice(dct.values())
-
 one = Label(root, text="Fun English A bit Everyday!"\
             , font=("Helvetica", 35), fg="#5f62d9").grid(row=0 ,column=1)
 #Photo for app
@@ -19,28 +57,30 @@ bubble = PhotoImage(file="bubble.gif")
 pic_1 = Label(root, image=avatar).grid(row=2, column=0)
 pic_2 = Label(root, image=bubble).grid(row=2, column=1)
 
-def printcheck():
+def printcheck(answer, point):
     print type(answer)
     if answer.lower() in dct.keys():
-        print 'Correct'
+        answer_2 = Message(root, text="                         "\
+                    , font=("Helvetica", 19)\
+                , fg="#5f62d9", width=200, justify=LEFT, anchor=N).grid(row=2 ,column=1)
+        wrd = random.choice(dct.values())
+        answer_2 = Message(root, text="%s" % (wrd)\
+                    , font=("Helvetica", 19)\
+                , fg="#5f62d9", width=200, justify=LEFT, anchor=N).grid(row=2 ,column=1)
     else:
-        print 'kakkkkkkkkkkkkkkk'
-    
+        print 'kakkkkkkkkkkkkkkk'    
 
 #Box to type + sent button
-answer = Entry(root).grid(row=3, column=1)
+point = Entry(root)        
+answer = Entry(root)
+answer.grid(row=3, column=1)
 sent = Button(root, text="Sent!", font=("Helvetica", 14)\
-              , fg="#5f62d9", bg="#e8f5ff", command=printcheck)
+              , fg="#5f62d9", bg="#e8f5ff", command= lambda: printcheck(answer.get(), point))
 sent.grid(row=4, column=1)
-
+point = 0
 #Answer box 
-answer = Message(root, text="%s" % (wrd)\
+answer_2 = Message(root, text="%s" % (wrd)\
                  , font=("Helvetica", 15)\
                , fg="#5f62d9", width=200, justify=LEFT, anchor=N).grid(row=2 ,column=1)
 
-#Quit button
-qui = Button(root, text="Quit", font=("Helvetica", 14)\
-              , fg="#5f62d9", bg="#e8f5ff", command=root.destroy)
-qui.grid(row=4, column=2)
-
-root.mainloop()
+mainloop()
